db40b1ab3232ec0ec3db1a7c7675a2668669a07f
jjlucot.dev <jjlucot.dev@gmail.com>
2024-06-08 21:47:39 +0800

    new updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
-
 const cors = require('cors');
 const userRoutes = require('./routes/user');
 const productRoutes = require('./routes/product');
@@ -8,23 +9,24 @@ const cartRoutes = require('./routes/cart');
 const orderRoutes = require('./routes/order');
 
 const app = express();
-const port = 4005;
+const port = process.env.PORT || 4005;
 
-//jason connection
-mongoose.connect("mongodb+srv://admin:admin1234@capstone2-ecommerce.x4q92t5.mongodb.net/ecommerce?retryWrites=true&w=majority");
-
-//jose connection
-// mongoose.connect("mongodb+srv://admin:admin1234@wdc028-course-booking.smrreb4.mongodb.net/ecommerce?retryWrites=true&w=majority");
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch(error => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
 let db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("We're connected to the cloud database"));
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use("/b5/users", userRoutes);
@@ -32,8 +34,14 @@ app.use("/b5/products", productRoutes);
 app.use("/b5/cart", cartRoutes);
 app.use("/b5/orders", orderRoutes);
 
-if(require.main === module){
-	app.listen(process.env.PORT || port, () => console.log(`API is now online on port ${process.env.PORT || port}`));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+if (require.main === module) {
+  app.listen(process.env.PORT || port, () => console.log(`API is now online on port ${process.env.PORT || port}`));
 }
 
-module.exports = {app, mongoose};
+module.exports = { app, mongoose };
